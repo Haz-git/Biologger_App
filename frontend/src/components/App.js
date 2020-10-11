8d8bad3d22118809ef11401b3ed9aa7916a430c9
Haz-git <zhouharry1@gmail.com>
2020-10-10 17:44:42 -0700

    Structured Files

---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+//Dependencies
+import React from 'react';
+
+//Components
+import Navbar from './Navbar';
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>This is where the navbar should be</div>
+    )
+}
+
+export default Navbar;
