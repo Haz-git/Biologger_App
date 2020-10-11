0667714789a8119a1fe69b16cac16daf11ad39a7
Haz-git <zhouharry1@gmail.com>
2020-10-11 06:42:08 -0700

    Rudimentary NavBar Completed

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
+//Styling:
+
 const StyledNavbar = styled.div`
+    display: flex;
+    justify-content: space-between;
     background-color: salmon;
+`
+
+const CenterNavbar = styled.div`
+    display: flex;
+    justify-content: space-around;
 
 `
+const StyledLink = styled.a`
+    text-decoration: none;
+    padding: 20px 20px;
+    &:hover {
+        background: lightblue;
+    }
+`
+
+//Component Structure:
 
 const Navbar = () => {
     return (
         <StyledNavbar>
-            <a href='#'>Logo/Home</a>
+            <StyledLink href='#'>Logo/Home</StyledLink>
+            <CenterNavbar>
+                <StyledLink href='#'>About Biologger</StyledLink>
+                <StyledLink href='#'>Support and Services</StyledLink>
+            </CenterNavbar>
+            <StyledLink href='#'>Login</StyledLink>
         </StyledNavbar>
     )
 }
