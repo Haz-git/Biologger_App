c566a24b8a65acb4f2392db505deb239946c8a31
Haz-git <zhouharry1@gmail.com>
2020-10-19 08:55:47 -0700

    Server is Online

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,16 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
 
+//Using dotenv for env variables:
+dotenv.config({
+    path: `${__dirname}/config.env`
+});
+//Use Json middleware:
+app.use(express.json());
+
+//Server Start:
+app.listen(process.env.PORT, () => {
+    console.log(`The server is online and listening on port: ${process.env.PORT}`);
+})
 
