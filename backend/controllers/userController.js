25f6261cdc7cb2a081ecb3fb2c106fd6e70671b7
Haz-git <zhouharry1@gmail.com>
2020-10-19 09:53:54 -0700

    Created Custom Error Handler and Async Handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,14 @@
-exports.signup = (req, res) => {
+const handleAsync = require('../utils/handleAsync');
+
+exports.signup = handleAsync( async(req, res, next) => {
+
+
+
+
     res.status(200).json({
         Message: "This route should create a new account and sign you up",
     })
-}
+});
 
 exports.login = (req, res) => {
     res.status(200).json({
