fbbe081bd76dab3febc3dcd132dcd3ea0621dccc
Haz-git <zhouharry1@gmail.com>
2020-10-19 09:20:17 -0700

    Connected Server to MongoDB via Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 //Using dotenv for env variables:
 dotenv.config({
@@ -7,6 +8,19 @@ dotenv.config({
 
 //Grabbing Express Application: 
 const app = require('./app.js');
+
+//Connecting app to MongoDB via Mongoose:
+
+mongoose
+    .connect(process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD), {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(() => {
+        console.log(`Biologger App on port: ${process.env.PORT} has been connected to MongoDB Atlas`);
+    })
 
 //Server Start:
 app.listen(process.env.PORT, () => {
