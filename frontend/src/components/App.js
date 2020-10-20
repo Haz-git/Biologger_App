1439dbcbfb24043820deebb0d6c6a75353c6111b
Haz-git <zhouharry1@gmail.com>
2020-10-19 18:27:49 -0700

    Created History Object and using React-Router-DOM.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,17 +1,24 @@
 //Dependencies
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 //Components
 import Navbar from './landingPage/Navbar';
 import MainBackdrop from './landingPage/MainBackdrop';
 import MainBodyblock from './landingPage/MainBodyblock';
+import Footer from './landingPage/Footer';
+import signupForm from './signupPage/signupForm';
 
 const App = () => {
     return (
         <div>
-            <Navbar />
-            <MainBackdrop />
-            <MainBodyblock />
+            <BrowserRouter>
+                <Navbar />
+                <Route exact path='/signup' component={signupForm} />
+                <MainBackdrop />
+                <MainBodyblock />
+                <Footer />
+            </BrowserRouter>
         </div>
     )
 }
---
+++ frontend/src/components/signupPage/signupForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const signupForm = () => {
+    return (
+        <div>This should be the signup Form</div>
+    )
+}
+
+export default signupForm;
