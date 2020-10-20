c1961dbdbfcc9634b091be736b1fc8f8c1f1d1e0
Haz-git <zhouharry1@gmail.com>
2020-10-19 17:35:37 -0700

    Adding Login logic

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,12 +1,20 @@
-const handleAsync = require('../utils/handleAsync');
+//Modules:
 const bcrypt = require('bcryptjs');
-const User = require('../models/userModels');
 const jwt = require('jsonwebtoken');
+
+//Model:
+const User = require('../models/userModels');
+
+//Utilities:
+const handleAsync = require('../utils/handleAsync');
+const throwAppError = require('../utils/throwAppError');
 
 //Creating function to sign JWT:
 const signToken = id => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
 }
+
+//Sign up Controller:
 
 exports.signup = handleAsync(async(req, res, next) => {
 
@@ -43,8 +51,31 @@ exports.signup = handleAsync(async(req, res, next) => {
     });
 });
 
-exports.login = (req, res) => {
+//Login Controller:
+
+exports.login = handleAsync(async (req, res, next) => {
+
+    //1. Grab email and password from request object. Check if email and password exist.
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(new throwAppError('Please enter your email and password', 400));
+    }
+
+    //2. Find email from documents in mongodb. Grab user with email and password.
+
+    const user = await User.findOne({ email }).select('+password');
+
+    //3. Use instance method to hash inputted password and compare with stored password to see if correct:
+    
+
+    //4. If match, send Token to client.
+
+    if (inputPassword === user.password) {
+        
+    }
+
     res.status(200).json({
         Message: "This should log you back in",
     })
-}
+});
