53d824a7b3523e0f62a4c2110200c32d5f8c5b1a
Haz-git <zhouharry1@gmail.com>
2020-10-21 09:37:56 -0700

    Adding Function to Create JWT Cookie for client

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -14,6 +14,31 @@ const signToken = id => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
 }
 
+//Function to send token via cookie:
+const createSendToken = (user, statusCode, res) => {
+    const token = signtoken(user._id);
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+        cookieOptions.secure = true;
+    }
+
+    res.cookie('jwt', token, cookieOptions);
+
+    res.status(statusCode).json({
+        status: 'Success',
+        token,
+        data: {
+            user
+        }
+    })
+}
+
 //Sign up Controller:
 
 exports.signup = handleAsync(async(req, res, next) => {
