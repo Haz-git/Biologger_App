6b1abc2ef75dd6e0454e24673407e07e560b8a46
Haz-git <zhouharry1@gmail.com>
2020-10-28 14:58:00 -0700

    Created Foundational Rich Text Editor

--- frontend/src/components/Dashboard/CreateBioNote.js
+++ frontend/src/components/Dashboard/CreateBioNote.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Editor, EditorState } from 'draft-js';
+import styled from 'styled-components';
 
-const CreateBioNote = () => {
-    return (
-        <div>
-            This should be the page for creating a bionote
-        </div>
-    )
+//Styles:
+const MainContainer = styled.div`
+    background-color: pink;
+    border: 1px solid gray;
+`
+
+
+//Render:
+
+class CreateBioNote extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editorState: EditorState.createEmpty(),
+        }
+    }
+
+    onChange = (editorState) => {
+        this.setState({
+            editorState
+        });
+    }
+
+    render() {
+        return (
+            <MainContainer>
+                <Editor 
+                    editorState={this.state.editorState} 
+                    onChange={this.onChange} 
+                />
+            </MainContainer>
+        )
+    }
 }
 
 export default CreateBioNote;
