77abd1e07626576dde5f578cf72dce65ab4e9bf3
Haz-git <zhouharry1@gmail.com>
2020-10-28 15:07:26 -0700

    Added rich text options

--- frontend/src/components/Dashboard/CreateBioNote.js
+++ frontend/src/components/Dashboard/CreateBioNote.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 import styled from 'styled-components';
 
 //Styles:
 const MainContainer = styled.div`
-    background-color: pink;
     border: 1px solid gray;
+`
+const OptionsContainer = styled.div`
+    padding: 10px 10px;
+`
+const EditorContainer = styled.div`
+    padding: 10px 10px;
+    border: 1px solid black;
 `
 
 
@@ -25,13 +31,44 @@ class CreateBioNote extends Component {
         });
     }
 
+    handleKeyCommand = (command) => {
+        const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+
+        if (newState) {
+            this.onChange(newState);
+            return 'handled';
+        }
+
+        return 'not-handled';
+    }
+
+    onUnderlineClick = () => {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+    }
+
+    onBoldClick = () => {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+    }
+    
+    onItalicClick = () => {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+    }
+
     render() {
         return (
             <MainContainer>
-                <Editor 
-                    editorState={this.state.editorState} 
-                    onChange={this.onChange} 
-                />
+                <OptionsContainer>
+                    <button onClick={this.onUnderlineClick}>U</button>
+                    <button onClick={this.onBoldClick}><b>B</b></button>
+                    <button onClick={this.onItalicClick}><em>I</em></button>
+                </OptionsContainer>
+                <EditorContainer>
+                    <Editor 
+                        editorState={this.state.editorState}
+                        handleKeyCommand={this.handleKeyCommand}
+                        onChange={this.onChange} 
+                    />
+                </EditorContainer>
             </MainContainer>
         )
     }
