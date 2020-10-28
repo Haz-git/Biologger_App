3f1f8a3e9668fe03362825d3090db261c9ac5b24
Haz-git <zhouharry1@gmail.com>
2020-10-28 13:37:55 -0700

    Created Class Component To Render User Details

--- frontend/src/components/Dashboard/MainDashboard.js
+++ frontend/src/components/Dashboard/MainDashboard.js
@@ -1,20 +1,47 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import { getJWT } from '../../utils/jwthelper';
 
-const MainDashboard = ({ user }) => {
-    return (
-        <div>
-            <h1>Welcome back, {user.firstName}!</h1>
-            <h2>You are currently signed in under {user.email}</h2>
-        </div>
-    )
-}
+class MainDashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-const mapStateToProps = state => {
-    return {
-        user: state.auth.userLogIn.data,
+    componentDidMount() {
+        const userDetails = getJWT();
+
+        const { firstName, lastName, userName, email } = userDetails.data;
+
+        this.setState({
+            firstName,
+            lastName,
+            userName,
+            email,
+        });
+
+        console.log(getJWT());
+    }
+
+    render() {
+
+        const { firstName, lastName, userName, email } = this.state;
+
+        return (
+            <div>
+                <h1>Welcome back {firstName} </h1>
+                <h2>You are currently signed in under {email}</h2>
+            </div>
+        )
     }
 }
 
+// const mapStateToProps = state => {
+//     return {
+//         user: state.auth.userLogIn.data,
+//     }
+// }
 
-export default connect(mapStateToProps)(MainDashboard);
+
+// export default connect(mapStateToProps)(MainDashboard);
+
+export default MainDashboard;
