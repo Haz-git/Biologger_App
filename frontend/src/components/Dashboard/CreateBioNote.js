0b5d8749c70ec957c6a51a36d2fb14e0819e1683
Haz-git <zhouharry1@gmail.com>
2020-10-29 14:22:41 -0700

    Removed Draft-js Components. Performance too low

--- frontend/src/components/Dashboard/CreateBioNote.js
+++ frontend/src/components/Dashboard/CreateBioNote.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
-import { EditorState, RichUtils } from 'draft-js';
-import Editor from 'draft-js-plugins-editor';
 import styled from 'styled-components';
 
-//Plugins:
-import createHighlightPlugin from './bionotePlugins/bionoteHighlightPlugin';
-import addLinkPlugin from './bionotePlugins/addLinkPlugin';
 
 //Styles:
 const MainContainer = styled.div`
@@ -19,99 +14,14 @@ const EditorContainer = styled.div`
     border: 1px solid black;
 `
 
-//Generating highlight Plugin:
-const highlightPlugin = createHighlightPlugin();
-
 
 //Render:
 
-class CreateBioNote extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            editorState: EditorState.createEmpty(),
-        }
-
-        this.plugins = [
-            highlightPlugin,
-            addLinkPlugin,
-        ];
-    }
-
-    onChange = (editorState) => {
-        this.setState({
-            editorState
-        });
-    }
-
-    handleKeyCommand = (command) => {
-        const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
-
-        if (newState) {
-            this.onChange(newState);
-            return 'handled';
-        }
-
-        return 'not-handled';
-    }
-
-    onUnderlineClick = () => {
-        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
-    }
-
-    onBoldClick = () => {
-        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-    }
-    
-    onItalicClick = () => {
-        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
-    }
-
-    onHighlight = () => {
-        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'HIGHLIGHT'))
-    }
-
-    onAddLink = () => {
-        const editorState = this.state.editorState;
-        const selection = editorState.getSelection();
-        const link = window.prompt('Paste the link -')
-
-        if (!link) {
-            this.onChange(RichUtils.toggleLink(editorState, selection, null));
-            return 'handled';
-        }
-
-        const content = editorState.getCurrentContent();
-        const contentWithEntity = content.createEntity('LINK', 'MUTABLE', { url: link });
-        const newEditorState = EditorState.push(editorState, contentWithEntity, 'create-entity');
-        const entityKey = contentWithEntity.getLastCreatedEntityKey();
-        this.onChange(RichUtils.toggleLink(newEditorState, selection, entityKey))
-
-    }
-
-    render() {
-        return (
-            <MainContainer>
-                <OptionsContainer>
-                    <button onClick={this.onUnderlineClick}>U</button>
-                    <button onClick={this.onBoldClick}><b>B</b></button>
-                    <button onClick={this.onItalicClick}><em>I</em></button>
-                    <button className="highlight" onClick={this.onHighlight}>
-                        <span style={{ background: "yellow" }}>H</span>
-                    </button>
-                    <button onClick={this.onAddLink}>Add Link</button>
-                </OptionsContainer>
-                <EditorContainer>
-                    <Editor 
-                        editorState={this.state.editorState}
-                        handleKeyCommand={this.handleKeyCommand}
-                        onChange={this.onChange}
-                        plugins={this.plugins}
-                    />
-                </EditorContainer>
-            </MainContainer>
-        )
-    }
+const CreateBioNote = () => {
+    return (
+        <div>Deleted</div>
+    )
 }
+
 
 export default CreateBioNote;
