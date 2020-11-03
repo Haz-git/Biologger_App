5bde5aa5227266acffb392bced322f7a981365a2
Haz-git <zhouharry1@gmail.com>
2020-11-03 14:06:15 -0800

    Added userName tag to message

--- frontend/src/components/Dashboard/ChatCard.js
+++ frontend/src/components/Dashboard/ChatCard.js
@@ -4,7 +4,8 @@ const ChatCard = (props) => {
     return (
         <div>
             <div>
-                <h2>{props.sender.username}</h2>
+                <h2>{props.sender.userName}</h2>
+                <h3>{props.sender.createdAt}</h3>
             </div>
             <div>{props.message}</div>
         </div>
