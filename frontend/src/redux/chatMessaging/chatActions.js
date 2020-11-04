02b79146b073571c8b874d91301ee15687987264
Haz-git <zhouharry1@gmail.com>
2020-11-04 08:06:14 -0800

    Fixed incorrect usage of concat()-- live reload works

--- frontend/src/redux/chatMessaging/chatActions.js
+++ frontend/src/redux/chatMessaging/chatActions.js
@@ -17,28 +17,14 @@ export function restoreChats() {
     }
 }
 
-// export const updateStateAfterNewMessage = msg => {
-
-//     return {
-//         type: USER_CHAT_RECEIVED,
-//         payload: msg,
-//     }
-// }
 
 export function updateStateAfterNewMessage(data) {
     return (dispatch, getState) => {
         const { chat } = getState();
-        //grabbing chat state and concating prior to dispatch to reducer. This doesn't seem to be the problem.
-        console.log(chat);
-        console.log(chat.chatLogs.data.data.chats);
+
         const newChatLog = {...chat.chatLogs};
-        console.log('This is the newChatLog object', newChatLog);
-        newChatLog.data.data.chats.concat(data);
+        newChatLog.data.data.chats = newChatLog.data.data.chats.concat(data);
         console.log('After Appending: ', newChatLog)
-
-        //I need to figure out a way to remove chatLog, because when appending to reducer it has chatLog nested twice...
-
-        //Testing old way:
 
         dispatch({
             type: USER_CHAT_RECEIVED,
