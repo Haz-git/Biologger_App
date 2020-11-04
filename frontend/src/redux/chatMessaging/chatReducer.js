f3b8aa95e9fb93a23fec180e426bc2e6947d0686
Haz-git <zhouharry1@gmail.com>
2020-11-03 16:25:43 -0800

    Implemented Chat message type const and chat reducer

--- frontend/src/redux/chatMessaging/chatReducer.js
+++ frontend/src/redux/chatMessaging/chatReducer.js
@@ -1,4 +1,4 @@
-import { USER_CHAT_REQUEST } from './chatTypes';
+import { USER_CHAT_RECEIVED, USER_CHAT_REQUEST } from './chatTypes';
 
 const initialState = {};
 
@@ -6,6 +6,8 @@ export default (state = initialState, action) => {
     switch(action.type) {
         case USER_CHAT_REQUEST:
             return {...state, chatLogs: action.payload }
+        case USER_CHAT_RECEIVED:
+            return {...state, chatLogs: action.payload } //This is working fine...
         default:
             return state;
     }
