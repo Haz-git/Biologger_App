77fabe0f7d676d65a49247adbe0a943731030b2f
Haz-git <zhouharry1@gmail.com>
2020-11-06 05:35:37 -0800

    Generated basic task template for testing

--- frontend/src/components/Dashboard/MDTodo.js
+++ frontend/src/components/Dashboard/MDTodo.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const MDTodo = () => {
+
+    const [task, setTask] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(task + 'Has been submitted');
+    }
+
+    const handleChange = e => {
+        e.preventDefault();
+        console.log(e.target.value);
+        setTask(e.target.value);
+    }
+
     return (
         <>
             <h2>Task Log</h2>
             <div>
-                <h3>You currently have no tasks. Relax!</h3>
+                <div>
+                    Tasks should be here.
+                </div>
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <input onChange={handleChange}type='text' placeholder='Add New Task'></input>
+                        <button type='submit'>Submit</button>
+                    </form>
+                </div>
             </div>
         </>
     )
