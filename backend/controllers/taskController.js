4aa54ac1debdf928067709e42ae058b2e7066908
Haz-git <zhouharry1@gmail.com>
2020-11-06 08:56:15 -0800

    Added getTask function

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -3,10 +3,8 @@ const handleAsync = require("../utils/handleAsync");
 
 //Model:
 const User = require('../models/userModels');
-const { updateOne } = require("../models/userModels");
 
 exports.addTask = handleAsync(async(req, res) => {
-    //Find user model, first what's in our request object?
     //request object sends: { data: 'test' }
     const { data, _id } = req.body;
     //We have appended the data and the _id of user to the req.body. We can use the _id to find the right user to append the new task to.
@@ -22,13 +20,19 @@ exports.addTask = handleAsync(async(req, res) => {
     //The new task should be appended to user document:
 
     res.status(200).json({
-        status: 'The addTask controller function is working.',
+        status: 'Success',
         newAppendedTask: data,
     });
 });
 
 exports.getTasks = handleAsync(async(req, res) => {
+    const { _id } = req.body;
+
+    const existingUserTaskList = await User.findOne({ _id }).select('taskList');
+    console.log(existingUserTaskList);
+
     res.status(200).json({
-        status: 'The getTasks controller function is working.'
+        status: 'Success',
+        existingUserTaskList: existingUserTaskList.tastList,
     });
 });
