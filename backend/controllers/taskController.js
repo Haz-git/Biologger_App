e8aab506dd4bb1c8539d13b9f5f2d494aa5a5095
Haz-git <zhouharry1@gmail.com>
2020-11-06 06:01:28 -0800

    Testing req.body for id and message

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -5,6 +5,13 @@ const handleAsync = require("../utils/handleAsync");
 const User = require('../models/userModels');
 
 exports.addTask = handleAsync(async(req, res) => {
+    //Find user model, first what's in our request object?
+    //request object sends: { data: 'test' }
+    console.log(req.body);
+    //We have appended the data and the _id of user to the req.body. We can use the _id to find the right user to append the new task to.
+
+    //Find user model:
+
     res.status(200).json({
         status: 'The addTask controller function is working.'
     });
@@ -15,3 +22,5 @@ exports.getTasks = handleAsync(async(req, res) => {
         status: 'The getTasks controller function is working.'
     });
 });
+
+/* Hey! You left off here last night. Try to implement a feature to get into the user model, find the correct user model (maybe using the JWT? Does it work like that?) and append a NEW task into an array of tasks. This way, all tasks are unique to each profile. We can then return all the tasks to be mapped out and rendered in the front end. The challenge here is to verify/ find which user is adding to his/her task! */
