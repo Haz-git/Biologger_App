f48eebd24b7642b7c166f44105459f8d4d581fed
Haz-git <zhouharry1@gmail.com>
2020-11-06 08:25:22 -0800

    Implemented rudimentary getTasks(), retrieve array of all tasks from MDB.

--- frontend/src/redux/userTaskLog/userTaskLogActions.js
+++ frontend/src/redux/userTaskLog/userTaskLogActions.js
@@ -1,13 +1,26 @@
 import api from '../../api';
 import { USER_NEW_TASK, USER_GET_TASKS } from './userTaskLogTypes';
 
+export function getTasks() {
+    return async (dispatch) => {
+        const response = await api.get('/users/task');
+
+        console.log('Response from server for all tasks' + response);
+
+        dispatch({
+            type: USER_GET_TASKS,
+            payload: response.data
+        })
+    }
+}
+
 export function addNewTask(data) {
     return async (dispatch, getState) => {
         const { auth: { userLogIn: { data: { _id } }} } = getState();
 
         const response = await api.post(`/users/task`, {data, _id});
 
-        console.log('Response from server on newTaskAdd' + response);
+        console.log('Response from server on newTaskAdd ' + JSON.stringify(response));
 
         dispatch({
             type: USER_NEW_TASK,
