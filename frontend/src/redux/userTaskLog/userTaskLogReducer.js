09131c8df1c7056894b0b1769681b81b1430825c
Haz-git <zhouharry1@gmail.com>
2020-11-06 05:48:27 -0800

    Created testing task reducer

--- frontend/src/redux/userTaskLog/userTaskLogReducer.js
+++ frontend/src/redux/userTaskLog/userTaskLogReducer.js
@@ -1,1 +1,14 @@
 import { USER_NEW_TASK, USER_GET_TASKS } from './userTaskLogTypes';
+
+const initialState = {};
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case USER_NEW_TASK:
+            return {...state, Tasks: action.payload};
+        case USER_GET_TASKS:
+            return {...state, Tasks: action.payload};
+        default:
+            return state;
+    }
+}
