09705895457d8032cfc4ba7297427bbe0aa5bd5d
Haz-git <zhouharry1@gmail.com>
2020-11-08 06:46:31 -0800

    Connected TaskCard to redux for deletion function

--- frontend/src/components/Dashboard/TaskCard.js
+++ frontend/src/components/Dashboard/TaskCard.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
+import { deleteTask } from '../../redux/userTaskLog/userTaskLogActions';
 
 //Icons:
 import { FaCheckCircle } from 'react-icons/fa';
@@ -22,7 +24,7 @@ const StyledButton = styled.button`
     }
 `
 
-const TaskCard = ({ item }) => {
+const TaskCard = ({ item, deleteTask }) => {
 
     const [ status, setStatus ] = useState('incomplete');
 
@@ -32,7 +34,9 @@ const TaskCard = ({ item }) => {
     }
 
     const handleDelete = () => {
-        console.log('Task should have action creator to be deleted now...');
+        //item refers to own item
+        deleteTask(item);
+        console.log(item + ' Should be deleted');
     }
 
     const handleRefresh = () => {
@@ -81,4 +85,13 @@ const TaskCard = ({ item }) => {
     )
 }
 
-export default TaskCard;
+export default connect(null, { deleteTask })(TaskCard);
+
+/* log:11/7 
+
+So now you have a responsive (and quite shitty) button. However, the style can always be improved after. What you need to do now is to:
+1. create a deleteTask action creator that will send a POST request with your user ID to mongo
+2. Create a deleteTask route in mongo, find the USER document, and delete the task with the same name, and save it. Response should be the updated taskList with the task deleted.
+3. Dispatch the updated TaskList with response deleted. Create a case in the reducer and (remember, another action TYPE) to save and change the state.
+
+*/
