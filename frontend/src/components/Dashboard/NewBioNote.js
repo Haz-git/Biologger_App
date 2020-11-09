f5beef0a9262700ae9cfd31dab6bf9dd6eee6c5f
Haz-git <zhouharry1@gmail.com>
2020-11-09 14:00:38 -0800

    Implemented Quill editor

--- frontend/src/components/Dashboard/NewBioNote.js
+++ frontend/src/components/Dashboard/NewBioNote.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React from 'react';
+import ReactQuill from 'react-quill';
+
+
+const modules = {
+    toolbar: [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+        ['clean']                                         // remove formatting button
+    ]
+}
+
+const formats = [
+    'font',
+    'size',
+    'bold', 'italic', 'underline',
+    'list', 'bullet',
+    'align',
+    'color', 'background'
+];
 
 const NewBioNote = () => {
+
     return (
-        <div>
-            This should be a page where you can create a new bionote.
-        </div>
+        <>
+            <ReactQuill 
+                theme='snow'
+                modules={modules}
+                toolbar={formats}
+            />
+        </>
     )
 }
 
