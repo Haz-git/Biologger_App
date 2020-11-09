a6b329286959cad0afeb17c256b90ce41a7423a3
Haz-git <zhouharry1@gmail.com>
2020-11-09 13:09:51 -0800

    Created Component for rendering bionnote

---
+++ frontend/src/components/Dashboard/NewBioNote.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewBioNote = () => {
+    return (
+        <div>
+            This should be a page where you can create a new bionote.
+        </div>
+    )
+}
+
+export default NewBioNote;
