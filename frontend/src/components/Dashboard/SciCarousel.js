7dd65838e0c3c6f7699091b02e2622207c471aae
Haz-git <zhouharry1@gmail.com>
2020-11-09 10:19:20 -0800

    Implemented react-slice carousel and integrated with redux state to render SciCards

---
+++ frontend/src/components/Dashboard/SciCarousel.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Slider from 'react-slick';
+import { connect } from 'react-redux';
+
+//Components:
+import SciCard from './SciCard';
+
+const SciCarousel = ({ news }) => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    }
+
+    const renderNews = () => (
+        news.news.data.articles.map(article => (
+            <SciCard
+                source={article.source.name}
+                author={article.author}
+                title={article.title}
+                description={article.description}
+                url={article.url}
+                img={article.urlToImage}
+                pubTime={article.publishedAt}
+            />
+        ))
+    )
+
+    return (
+        <>
+            <Slider {...settings}>
+                {renderNews()}
+            </Slider>
+        </>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        news: state.news,
+    }
+}
+
+export default connect(mapStateToProps)(SciCarousel);
