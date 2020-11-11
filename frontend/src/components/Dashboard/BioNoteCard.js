050237ac3cbb3437e1834e8c48a5e00b14685d4b
Haz-git <zhouharry1@gmail.com>
2020-11-11 10:05:05 -0800

    Added Links to helper components

--- frontend/src/components/Dashboard/BioNoteCard.js
+++ frontend/src/components/Dashboard/BioNoteCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 //Styles:
 const MainCardContainer = styled.div`
@@ -7,17 +8,51 @@ const MainCardContainer = styled.div`
     padding: 10px 10px;
     border: 1px solid gray;
 `
+const CardInfoContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+`
+const LinkContainer = styled.div`
+    display: flex;
+`
+
+const StyledLink = styled(Link)`
+    color: navy;
+    text-decoration: none;
+    margin-left: 10px;
+    background-color: lightcyan;
+    padding: 10px 10px;
+    border: 1px solid white;
+    border-radius: 5px;
+`
+
 const StyledTitle = styled.p`
     margin: 0;
-    font-size: 30px;
+    font-size: 20px;
 `
 
 const BioNoteCard = ({ name }) => {
     return (
         <MainCardContainer>
-            <StyledTitle>{name}</StyledTitle>
+            <CardInfoContainer>
+                <StyledTitle>{name}</StyledTitle>
+                <LinkContainer>
+                    <StyledLink to={`/readbionote/${name}`}>Read</StyledLink>
+                    <StyledLink to={`/editbionote/${name}`}>Edit</StyledLink>
+                    <StyledLink to={`/deletebionote/${name}`}>Delete</StyledLink>
+                </LinkContainer>
+            </CardInfoContainer>
         </MainCardContainer>
     )
 }
+
+/*Todo Implementations:
+1. Add View button --> Renders out bionote with formatting
+2. Add Delete button 
+3. Add Edit button --> Opens editor with button with text + formatting + button for saving.
+
+*/
 
 export default BioNoteCard;
