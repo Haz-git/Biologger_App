27168c86ba1b3972aa66d6fb2bdf8ea658ce635d
Haz-git <zhouharry1@gmail.com>
2020-11-11 08:25:25 -0800

    Included Bioname within POST request to server

--- frontend/src/redux/userBioNote/bionoteActions.js
+++ frontend/src/redux/userBioNote/bionoteActions.js
@@ -5,11 +5,11 @@ import {
     USER_UPDATE_BIONOTE,
 } from './bionoteTypes';
 
-export function createNewBioNote(data) {
+export function createNewBioNote(bioName, data) {
     return async (dispatch, getState) => {
         const { auth: { userLogIn: { data: { _id } } } } = getState();
 
-        const response = await api.post('/users/bionote/create', {_id, data});
+        const response = await api.post('/users/bionote/create', { _id, bioName, data });
         console.log(response);
     }
 }
