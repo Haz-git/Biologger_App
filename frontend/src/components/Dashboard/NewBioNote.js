761f0ce7a3ee84cef7ba14bff1fa4f6f8c31353b
Haz-git <zhouharry1@gmail.com>
2020-11-10 16:44:32 -0800

    Created temp states

--- frontend/src/components/Dashboard/NewBioNote.js
+++ frontend/src/components/Dashboard/NewBioNote.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import ReactQuill from 'react-quill';
 import styled from 'styled-components';
 import QuillEditor from '../../editor/QuillEditor';
 
@@ -56,6 +55,12 @@ const NewBioNote = () => {
 The main goal here is to:
 
 1. Learn how to save documents into mongoDB into each individual user.
+**WE ARE HERE**
+
+We've now learned how to save the user's writing temporarily in the content state. We still need to create routing to our backend that will push this document into the user's stuff. 
+
+https://www.youtube.com/watch?v=AgreDlNaUn4 @ timestamp 38:21 --> He's setting up the model necessary for creating the blog...
+
 2. Learn how to load documents into the loading component "createbionote" --> perhaps a simple listing pattern?
 
 3. Learn how to grab HTML info from mongodb and re-render HTML document upon clicking it.. 
