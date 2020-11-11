d9946ff0aad7c0f2785d65e1deb813fcd2a3c1e2
Haz-git <zhouharry1@gmail.com>
2020-11-11 08:52:15 -0800

    Completed bionote reducer

---
+++ frontend/src/redux/userBioNote/bionoteReducer.js
@@ -0,0 +1,20 @@
+import {
+    USER_ADD_BIONOTE,
+    USER_DELETE_BIONOTE,
+    USER_UPDATE_BIONOTE,
+} from './bionoteTypes';
+
+const initialState = {};
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case USER_ADD_BIONOTE:
+            return {...state, bionotes: action.payload};
+        case USER_DELETE_BIONOTE:
+            return {...state, bionotes: action.payload};
+        case USER_UPDATE_BIONOTE:
+            return {...state, bionotes: action.payload};
+        default:
+            return state;
+    }
+}
