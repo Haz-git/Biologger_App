ec3bd202ccc2d53e070ded9bafb344ea852a8ba0
Haz-git <zhouharry1@gmail.com>
2020-11-11 08:50:01 -0800

    Created controller function for adding new bionote

--- backend/controllers/bionoteController.js
+++ backend/controllers/bionoteController.js
@@ -5,12 +5,27 @@ const handleAsync = require("../utils/handleAsync");
 const User = require('../models/userModels');
 
 exports.addBioNote = handleAsync(async(req, res) => {
+
+    //Extract data:
+    const { data, _id, bioName } = req.body;
+
+    //Find Appropriate User and select bionotes array:
+    const userExistingBioNotesCollection = await User.findOne({ _id }).select('bionotes');
     
-    console.log(req.body);
+    //Update bionotes array:
+    userExistingBioNotesCollection.bionotes.push({bioName, data});
+
+    //Update User with new bionotes array:
+    await User.updateOne({ _id }, { bionotes: userExistingBioNotesCollection.bionotes }, { bypassDocumentValidation: true}, (err) => {
+        if (err) console.log(err);
+    });
+
+    //Find updated user again for response:
+    const userNewBioNotesCollection = await User.findOne({ _id }).select('bionotes');
 
 
     res.status(200).json({
         status: 'Success',
-        message: 'Connected Successfully to this route in the API'
+        userNewBioNotesCollection
     });
 })
