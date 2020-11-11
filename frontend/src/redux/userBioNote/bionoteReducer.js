fdf5758b691cfdc5451492ca56086dda2020846b
Haz-git <zhouharry1@gmail.com>
2020-11-11 11:53:32 -0800

    Refactored reducer to include USERGETNOTES

--- frontend/src/redux/userBioNote/bionoteReducer.js
+++ frontend/src/redux/userBioNote/bionoteReducer.js
@@ -2,6 +2,7 @@ import {
     USER_ADD_BIONOTE,
     USER_DELETE_BIONOTE,
     USER_UPDATE_BIONOTE,
+    USER_GET_BIONOTES,
 } from './bionoteTypes';
 
 const initialState = {};
@@ -9,6 +10,8 @@ const initialState = {};
 export default (state = initialState, action) => {
     switch(action.type) {
         case USER_ADD_BIONOTE:
+            return {...state, bionotes: action.payload};
+        case USER_GET_BIONOTES:
             return {...state, bionotes: action.payload};
         case USER_DELETE_BIONOTE:
             return {...state, bionotes: action.payload};
