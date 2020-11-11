eaa49741ccc240c441f8fce2d855a30b0111f36a
Haz-git <zhouharry1@gmail.com>
2020-11-11 06:05:00 -0800

    Moving from react-quill to react-draft-wysiwyg

--- frontend/src/components/Dashboard/NewBioNote.js
+++ frontend/src/components/Dashboard/NewBioNote.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import QuillEditor from '../../editor/QuillEditor';
+// import QuillEditor from '../../editor/QuillEditor';
 import { connect } from 'react-redux';
 import { createNewBioNote } from '../../redux/userBioNote/bionoteActions';
-import { create } from 'lodash';
+
+//Editor imports:
+import { Editor } from 'react-draft-wysiwyg';
+import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 //Styles:
 
@@ -21,21 +24,22 @@ const NewBioNote = ({ createNewBioNote }) => {
     const [content, setContent] = useState('');
     const [files, setFiles] = useState([]);
 
-    const onEditorChange = (value) => {
-        console.log(value);
-        setContent(value);
-    }
+    // const onEditorChange = (value) => {
+    //     console.log(value);
+    //     setContent(value);
+    // }
 
-    const onFileChange = (files) => {
-        console.log(files);
-        setFiles(files);
-    }
+    // const onFileChange = (files) => {
+    //     console.log(files);
+    //     setFiles(files);
+    // }
 
     const onEditorSubmit = (e) => {
         e.preventDefault();
-        console.log({content, files});
-        createNewBioNote({content, files});
+        // console.log({content, files});
+        // createNewBioNote({content, files});
     }
+
 
     return (
         <>
@@ -49,12 +53,7 @@ const NewBioNote = ({ createNewBioNote }) => {
                         />
                     </div>
                     <div>
-                        <QuillEditor
-                            theme='snow'
-                            placeholder='Start Writing Your BioNote...'
-                            onEditorChange={onEditorChange}
-                            onFileChange={onFileChange}
-                        />
+                        <Editor />
                     </div>
                     <button type='submit'>Submit BioNote</button>
                 </form>
@@ -71,9 +70,16 @@ The main goal here is to:
 
 We've now learned how to save the user's writing temporarily in the content state. We still need to create routing to our backend that will push this document into the user's stuff. 
 
-https://www.youtube.com/watch?v=AgreDlNaUn4 @ timestamp 38:21 --> He's setting up the model necessary for creating the blog...
+https://www.youtube.com/watch?v=AgreDlNaUn4 @ timestamp 38:21 --> He's setting up the model necessary for creating the blog...--> this model is only necessary for when we 'share' the blogs to the people on the app.
+
+**Update: We've currently implemented basic routing and had our _id and contents/file array to be sent over to the backend. It remains unknown whether we can just store this content data 'as is' and then use it to render it back out onto the screen...
+
+**Update: We might have to ditch react-quill much to my dismay. The documentation is pretty nasty, and it doesn't really give good examples. I've found this to be better:
+https://jpuri.github.io/react-draft-wysiwyg/#/docs
 
 2. Learn how to load documents into the loading component "createbionote" --> perhaps a simple listing pattern?
+
+
 
 3. Learn how to grab HTML info from mongodb and re-render HTML document upon clicking it.. 
 4. 
