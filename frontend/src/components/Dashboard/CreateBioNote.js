61dcda4a105c96ee045fc222802499bca9d21df4
Haz-git <zhouharry1@gmail.com>
2020-11-11 09:14:26 -0800

    Added rudimentary rendering of created bionotes

--- frontend/src/components/Dashboard/CreateBioNote.js
+++ frontend/src/components/Dashboard/CreateBioNote.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { v4 as uuid } from 'uuid';
+
+//Components:
+import BioNoteCard from './BioNoteCard';
 
 //Render:
 
-const CreateBioNote = () => {
+const CreateBioNote = ({ bionotes }) => {
+
+    const renderNotes = () => (
+        bionotes.map(note => (
+            <BioNoteCard key={uuid()} name={note.bioName} />
+        ))
+    )
+
     return (
-        <>
+        <h1>
             <h1>Your Bio-Notes</h1>
             <div>
-                Your currently have no bionotes.
+                {renderNotes()}
             </div>
             <div>
                 <button>
                     <Link to='newbionote'>Create New Bionote</Link>
                 </button>
             </div>
-        </>
+        </h1>
     )
 }
 
+const mapStateToProps = state => {
+    return {
+        bionotes: state.bionotes.bionotes,
+    }
+}
 
-export default CreateBioNote;
+
+export default connect(mapStateToProps)(CreateBioNote);
