8b0d0e793250c23a95fa8b953fc83d8ac9620910
Haz-git <zhouharry1@gmail.com>
2020-11-12 05:59:34 -0800

    Added ability to render bionote onto page for viewing

--- frontend/src/components/Dashboard/ReadBioNote.js
+++ frontend/src/components/Dashboard/ReadBioNote.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { Editor, EditorState, convertFromRaw } from 'draft-js';
 
-const ReadBioNote = ({ id }) => {
-    console.log(id);
+const ReadBioNote = ({ match:{params:{id}}, bionotes }) => {
+    
+    const [ editorState, setEditorState ] = useState(EditorState.createEmpty());
+
+    useEffect(() => {
+        renderBioNote();
+    },[]);
+
+    const renderBioNote = () => {
+        const currentNote = bionotes.bionotes.find(x => x.bioName === id);
+
+        const jsonCurrentNote = JSON.parse(currentNote.data);
+
+        const contentState = convertFromRaw(jsonCurrentNote);
+
+        const editorState = EditorState.createWithContent(contentState);
+
+        setEditorState(editorState);
+        
+    }
     
     return (
         <div>
-            Place for reading bionotes
+            <Editor
+                editorState={editorState}
+                readOnly={true}
+            />
         </div>
     )
 }
 
-export default ReadBioNote
+const mapStateToProps = state => {
+    return {
+        bionotes: state.bionotes,
+    }
+}
+
+export default connect(mapStateToProps)(ReadBioNote);
