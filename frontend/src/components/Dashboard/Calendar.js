8125c54808d3e8ec0230f5d8f5c89380428cd116
Haz-git <zhouharry1@gmail.com>
2020-11-12 14:20:59 -0800

    Added Calendar Functionality

--- frontend/src/components/Dashboard/Calendar.js
+++ frontend/src/components/Dashboard/Calendar.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import styled from 'styled-components';
+
+//Styles:
+const MainCalendarContainer = styled.div`
+    padding: 20px 20px;
+`
 
 const Calendar = () => {
     return (
         <>
-            <FullCalendar
-                plugins={[ dayGridPlugin ]}
-                initialView='dayGridMonth'
-            />
+            <MainCalendarContainer>
+                <FullCalendar
+                    plugins={[ dayGridPlugin ]}
+                    initialView='dayGridMonth'
+                    schedulerLicenseKey='GPL-My-Project-Is-Open-Source'
+                    events={[
+                        { title: 'Anniversary', start: '2020-11-13', end: '2020-11-16'},
+                    ]}
+                />
+            </MainCalendarContainer>
         </>
     )
 }
+
+/*
+Documentation:
+https://fullcalendar.io/docs#toc
+
+
+It seems like the events object can easily be saved to mongoDB for persistence... --> Using this strategy we can implement personal calendar..
+
+*/
 
 export default Calendar;
