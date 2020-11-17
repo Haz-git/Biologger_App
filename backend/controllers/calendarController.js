edbff7593993466f12c572bacd22efe84f8e5ba7
Haz-git <zhouharry1@gmail.com>
2020-11-17 08:13:48 -0800

    Added Controller Function for event deletion

--- backend/controllers/calendarController.js
+++ backend/controllers/calendarController.js
@@ -34,3 +34,28 @@ exports.getAllEvents = handleAsync(async (req, res) => {
         existingUserCalendarEvents: existingUserCalendarEvents.calendarEvents,
     })
 })
+
+exports.deleteEvent = handleAsync(async (req,res) => {
+    const { _id, event } = req.body;
+
+    const existingUserCalendarEvents = await User.findOne({ _id }).select('calendarEvents');
+
+    //Find matching event.id and delete from mongodb.
+
+    existingUserCalendarEvents.calendarEvents.splice(existingUserCalendarEvents.calendarEvents.indexOf(item => item.id === event.id),1);
+
+    //Save/update:
+
+    await User.updateOne({ _id }, { calendarEvents: existingUserCalendarEvents.calendarEvents }, { bypassDocumentValidation: true }, (err) => {
+        if (err) console.log(err)
+    });
+
+    //Send off updated calendarevents:
+
+    const updatedDeletedCalendarEvents = await User.findOne({ _id }).select('calendarEvents');
+
+    res.status(200).json({
+        status: 'Success',
+        updatedDeletedCalendarEvents,
+    })
+})
