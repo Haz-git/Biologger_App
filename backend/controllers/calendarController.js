8e89b13f137a2bc232e62dabb599f8de2a42a3da
Haz-git <zhouharry1@gmail.com>
2020-11-17 05:53:02 -0800

    Implemented addNewEvent controller function

--- backend/controllers/calendarController.js
+++ backend/controllers/calendarController.js
@@ -5,10 +5,21 @@ const handleAsync = require("../utils/handleAsync");
 const User = require('../models/userModels');
 
 exports.addNewEvent = handleAsync(async (req, res) => {
-    console.log(req.body);
+
+    const { _id, event } = req.body;
+    
+    const userCalendarEvents = await User.findOne({ _id }).select('calendarEvents');
+
+    userCalendarEvents.calendarEvents.push(event);
+
+    await User.updateOne({ _id }, { calendarEvents: userCalendarEvents.calendarEvents }, { bypassDocumentValidation: true}, (err) => {
+        if (err) console.log(err);
+    });
+
+    const userNewCalendarEvents = await User.findOne({ _id }).select('calendarEvents');
 
     res.status(200).json({
         status: 'Success',
-        message: 'This route is working!'
+        userNewCalendarEvents: userNewCalendarEvents.calendarEvents,
     })
 })
