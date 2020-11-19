a7c27084f371606ece15b1f5be48a04243274f1b
Haz-git <zhouharry1@gmail.com>
2020-11-19 08:12:16 -0800

    Created Rough ReStyling of form

--- frontend/src/components/signupPage/signupForm.js
+++ frontend/src/components/signupPage/signupForm.js
@@ -7,9 +7,16 @@ import userSignUp from '../../redux/userSignUp/userSignUpActions';
 //Styles:
 
 const MainContainer = styled.div`
-    background-color: white;
+    margin: 0 auto;
+    text-align: center;
+`
+
+const ContentContainer = styled.div`
+    display: flex;
+    background-color:#F6F9FC;
     padding: 20px 20px;
     text-align: center;
+
 `
 const HeaderContainer = styled.div`
     background-color: salmon;
@@ -27,13 +34,28 @@ const SecondaryHeader = styled.h2`
     font-family: 'Catamaran', sans-serif;
 `
 
-
 const FormContainer = styled.div`
-    display: grid;
-    grid-template-columns: 50% 50%;
+    flex-grow: 1;
+    text-align: center;
     background-color: lightblue;
     padding: 10px 10px;
 `
+
+const ImageContainer = styled.div`
+    flex-grow: 1;
+    background-color: navy;
+`
+
+const StyledField = styled(Field)`
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+`
+
 
 
 
@@ -47,46 +69,51 @@ const SignUpForm = ({ handleSubmit, userSignUp }) => {
     return (
         <>
             <MainContainer>
-                <HeaderContainer>
-                <MainHeader>Join Biologger.</MainHeader>
-                <SecondaryHeader>Create an account to increase your research workflow by writing bio-note snippets, setting task reminders, and organizing work schedule.</SecondaryHeader>
-                </HeaderContainer>
-                <form onSubmit={handleSubmit(dispatchFormValues)}>
-                    <FormContainer>
+                <ContentContainer>
+                    <ImageContainer>
+                        This should render an image.
+                    </ImageContainer>
+                    <form onSubmit={handleSubmit(dispatchFormValues)}>
+                        <FormContainer>
+                            <HeaderContainer>
+                                <MainHeader>Join Biologger.</MainHeader>
+                                <SecondaryHeader>Create an account to increase your research workflow by writing bio-note snippets, setting task reminders, and organizing work schedule.</SecondaryHeader>
+                            </HeaderContainer>
+                                <div>
+                                    <div>
+                                        <label>First Name</label>
+                                        <StyledField name='firstName' component='input'></StyledField>
+                                    </div>
+                                    <div>
+                                        <label>Last Name</label>
+                                        <StyledField name='lastName' component='input'></StyledField>
+                                    </div>
+                                    <div>
+                                        <label>Username</label>
+                                        <StyledField name='userName' component='input'></StyledField>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>
+                                        <label>Email Address</label>
+                                        <StyledField name='email' component='input'></StyledField>
+                                    </div>
+                                    <div>
+                                        <label>Password</label>
+                                        <StyledField name='password' component='input'></StyledField>
+                                    </div>
+                                    <div>
+                                        <label>Confirm Password</label>
+                                        <StyledField name='passwordConfirm' component='input'></StyledField>
+                                    </div>
+                                </div>
                             <div>
-                                <div>
-                                    <label>First Name</label>
-                                    <Field name='firstName' component='input'></Field>
-                                </div>
-                                <div>
-                                    <label>Last Name</label>
-                                    <Field name='lastName' component='input'></Field>
-                                </div>
-                                <div>
-                                    <label>Username</label>
-                                    <Field name='userName' component='input'></Field>
-                                </div>
+                                <button type='submit'>Submit</button>
+                                <button type='reset'>Reset</button>
                             </div>
-                            <div>
-                                <div>
-                                    <label>Email Address</label>
-                                    <Field name='email' component='input'></Field>
-                                </div>
-                                <div>
-                                    <label>Password</label>
-                                    <Field name='password' component='input'></Field>
-                                </div>
-                                <div>
-                                    <label>Confirm Password</label>
-                                    <Field name='passwordConfirm' component='input'></Field>
-                                </div>
-                            </div>
-                    </FormContainer>
-                    <div>
-                        <button type='submit'>Submit</button>
-                        <button type='reset'>Reset</button>
-                    </div>
-                </form>
+                        </FormContainer>
+                    </form>
+                </ContentContainer>
             </MainContainer>
         </>
     )
