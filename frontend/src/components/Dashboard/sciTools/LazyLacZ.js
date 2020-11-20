4234fb5d4683c3e9f45754c27508c7292457573a
Haz-git <zhouharry1@gmail.com>
2020-11-20 14:18:45 -0800

    Added LazyLacZ component

--- frontend/src/components/Dashboard/sciTools/LazyLacZ.js
+++ frontend/src/components/Dashboard/sciTools/LazyLacZ.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+
+//Styles:
+import { MainHeader, SecondaryHeader } from '../../signupPage/SignUpForm';
+import { AddCircle } from '@styled-icons/ionicons-solid/AddCircle';
+
+const MainContainer = styled.div`
+    text-align: center;
+    padding: 20px 20px;
+
+`
+
 
 const LazyLacZ = () => {
     return (
-        <div>This should be the lacz page</div>
+        <>
+            <MainContainer>
+                <MainHeader>Lac Z Assay</MainHeader>
+                <div>
+                    <SecondaryHeader>Add New Bacterial Strains</SecondaryHeader>
+                    <form>
+                    </form>
+                </div>
+            </MainContainer>
+        </>
     )
 }
 
