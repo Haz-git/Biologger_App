26d25dec6eb8b7d9b3915fca89da8bec5e39b7ab
Haz-git <zhouharry1@gmail.com>
2020-11-20 11:35:39 -0800

    Created SciTools landing for loading multiple sci apps

--- frontend/src/components/Dashboard/sciTools/SciToolsLanding.js
+++ frontend/src/components/Dashboard/sciTools/SciToolsLanding.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SciToolsLanding = () => {
     return (
         <div>
-            This should be the landing page for scitools!
+            <h1>This should be the landing page for SciTools</h1>
+            <h2>Once completed, It should render out useful Scitools in a grid(?) format</h2>
+            <Link to='/scitools/lazylacz'>
+                <button>
+                    Lazy LacZ
+                </button>
+            </Link>
         </div>
     )
 }
