ff6bfc4fde367829833b08aa86199a20315edd10
Haz-git <zhouharry1@gmail.com>
2020-11-21 06:54:09 -0800

    Implemented GET and POST controller functions

--- backend/controllers/laczController.js
+++ backend/controllers/laczController.js
@@ -7,15 +7,34 @@ const User = require('../models/userModels');
 //Controller Functions:
 
 exports.addNewStrain = handleAsync(async (req, res) => {
-    console.log(req.body);
+
+    const { strainName, _id } = req.body;
+
+    const userExistingStrains = await User.findOne({ _id }).select('laczAssay');
+
+    userExistingStrains.laczAssay.push({ strainName });
+
+    await User.updateOne({ _id }, { laczAssay: userExistingStrains.laczAssay }, { bypassDocumentValidation: true}, (err) => {
+        if (err) console.log(err);
+    });
+
+    const responseStrainList = await User.findOne({ _id }).select('laczAssay');
 
 
-    // res.status(200).json({
-    //     status: 'Success',
-
-    // })
+    res.status(200).json({
+        status: 'Success',
+        strainList: responseStrainList.laczAssay,
+    })
 })
 
 exports.getAllStrains = handleAsync(async (req, res) => {
-    console.log(req.body);
+
+    const { _id } = req.body;
+
+    const userGetStrainList = await User.findOne({ _id }).select('laczAssay');
+
+    res.status(200).json({
+        status: 'Success',
+        strainList: userGetStrainList.laczAssay,
+    })
 })
