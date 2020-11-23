4b35853d641b00c552c22b5afd98ced67c885475
Haz-git <zhouharry1@gmail.com>
2020-11-23 09:10:55 -0800

    Connected collection page to state

--- frontend/src/components/Dashboard/sciTools/Collection.js
+++ frontend/src/components/Dashboard/sciTools/Collection.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Collection = () => {
+const Collection = ({ownProtocol}) => {
+
+    //id == protocolId for mongoDB.
+    const { protocolName, protocolId, timeStamp } = ownProtocol;
+
     return (
-        <div>
-            collection page
-        </div>
+        <>
+            Collection Page for: {protocolName}, created on {timeStamp}...
+        </>
     )
 }
 
-export default Collection;
+const mapStateToProps = (state, ownProps) => {
+    const ownProtocolId = ownProps.match.params.id;
+    const ownProtocol = state.laczAssayProtocols.laczProtocol.find(item => item.protocolId === ownProtocolId);
+
+    return {
+        ownProtocol,
+    }
+
+}
+
+export default connect(mapStateToProps)(Collection);
