dbdcb1eb89a81e8090302c4c07c11a528637a58d
Haz-git <zhouharry1@gmail.com>
2020-11-23 14:27:13 -0800

    Creating card for strain loading

---
+++ frontend/src/components/Dashboard/sciTools/CollectionStrains.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+//Styles:
+import styled from 'styled-components';
+import { CollectionContainer } from './Collection';
+
+
+//Render:
+
+const CollectionStrains = () => {
+    return (
+        <>
+            <CollectionContainer>
+                <h1>Hi</h1>
+            </CollectionContainer>
+        </>
+    )
+}
+
+export default CollectionStrains;
