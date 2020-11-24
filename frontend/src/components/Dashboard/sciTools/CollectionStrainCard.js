67a45218ad54668394d838a5a0f7d7ae08864ace
Haz-git <zhouharry1@gmail.com>
2020-11-24 06:03:48 -0800

    Adding Card for collection render

---
+++ frontend/src/components/Dashboard/sciTools/CollectionStrainCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import Card from 'react-bootstrap/Card';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+//Create a card with an input group--might have to add starting OD600 to the collection input card...
+
+const CollectionStrainCard = ({ name, pointNum, startTime }) => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default CollectionStrainCard;
