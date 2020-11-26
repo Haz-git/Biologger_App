a9f8ac20da732507b705d58bb741783149dba4e8
Haz-git <zhouharry1@gmail.com>
2020-11-26 07:30:01 -0800

    Created parsedData object that renders minutes to the object retrieved from state

--- frontend/src/components/Dashboard/sciTools/CollectionCharts.js
+++ frontend/src/components/Dashboard/sciTools/CollectionCharts.js
@@ -19,27 +19,70 @@ const ChartContainer = styled.div`
 const CollectionCharts = ({ ownProtocolId, laczAssayProtocols }) => {
 
     const ownProtocol = laczAssayProtocols.find(item => item.protocolId === ownProtocolId);
-    console.log(ownProtocol.collectionStrains[0].collectionData);
+    console.log(ownProtocol.collectionStrains);
 
-    let parsedData;
-    
-    if (ownProtocol.collectionStrains.collectionData) {
-        parsedData = [...ownProtocol.collectionStrains.collectionData]
+    let parsedData = [...ownProtocol.collectionStrains];
+
+    console.log(parsedData);
+
+    if (parsedData !== undefined) {
+        
+        let startTime;
+
+        for (let i = 0; i < parsedData.length; i++) {
+
+            if (parsedData[i].collectionData) {
+
+                for (let j = 0; j < parsedData[i].collectionData.length; j++) {
+                    if (j === 0) {
+                        parsedData[i].collectionData[j]['timeMinutes'] = '0';
+                        startTime = parsedData[i].collectionData[j].time.split(':').map(x => parseInt(x));
+                    } else {
+                        let timeSplit = parsedData[i].collectionData[j].time.split(':').map(x => parseInt(x));
+                        let minuteDifference = ((timeSplit[0] - startTime[0]) * 60) + timeSplit[1];
+                        parsedData[i].collectionData[j]['timeMinutes'] = minuteDifference.toString();
+                    }
+                }
+
+            }
+        }
     }
 
     console.log(parsedData);
 
+    // if (parsedData !== undefined) {
+    //     let startTime;
+    //     for (let i = 0; i < parsedData.length; i++) {
+
+    //         if (i === 0) {
+    //             parsedData[i]['timeMinutes'] = '0';
+    //             startTime = parsedData[i].time.split(':').map(x => parseInt(x));
+    //         } else {
+    //             let timeSplit = parsedData[i].time.split(':').map(x => parseInt(x));
+    //             console.log(timeSplit);
+    //             console.log(startTime);
+    //             let minuteDifference = ((timeSplit[0] - startTime[0]) * 60) + timeSplit[1];
+    //             parsedData[i]['timeMinutes'] = minuteDifference.toString();
+    //         }
+
+    //     }
+    // }
+
+    // console.log(parsedData);
+    // ownProtocol.collectionStrains['collectionData'] = parsedData;
+    // console.log(ownProtocol.collectionStrains)
+
     const renderCharts = () => {
-        if (ownProtocol.collectionStrains) {
+        if (parsedData) {
             return (
-                ownProtocol.collectionStrains.map(item => (
+                parsedData.map(item => (
                     <>
                         <h3>{item.strainName}</h3>
                         <LineChart width={600} height={400} data={item.collectionData} margin={{ top: 10, right: 30, left: 30, bottom: 40 }}>
                             <Line type="monotone" dataKey="odValue" stroke="#8884d8" />
                             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                            <XAxis dataKey="time">
-                                <Label value='Time Collected' position='bottom' style={{ textAnchor: 'middle' }} />
+                            <XAxis dataKey="timeMinutes">
+                                <Label value='Minutes' position='bottom' style={{ textAnchor: 'middle' }} />
                             </XAxis>
                             <YAxis dataKey='odValue'>
                                 <Label value='OD600 Value' position='left' angle='-90' style={{ textAnchor: 'middle' }} />
