6df0ca9e245b07ae59eedc36458d2bdf5973c9f2
Haz-git <zhouharry1@gmail.com>
2020-11-26 07:45:44 -0800

    Created responsive container

--- frontend/src/components/Dashboard/sciTools/CollectionCharts.js
+++ frontend/src/components/Dashboard/sciTools/CollectionCharts.js
@@ -13,17 +13,15 @@ const ChartMainContainer = styled.div`
 `
 
 const ChartContainer = styled.div`
-
+    height: 500px;
+    width: 100%;
 `
 
 const CollectionCharts = ({ ownProtocolId, laczAssayProtocols }) => {
 
     const ownProtocol = laczAssayProtocols.find(item => item.protocolId === ownProtocolId);
-    console.log(ownProtocol.collectionStrains);
 
     let parsedData = [...ownProtocol.collectionStrains];
-
-    console.log(parsedData);
 
     if (parsedData !== undefined) {
         
@@ -48,29 +46,6 @@ const CollectionCharts = ({ ownProtocolId, laczAssayProtocols }) => {
         }
     }
 
-    console.log(parsedData);
-
-    // if (parsedData !== undefined) {
-    //     let startTime;
-    //     for (let i = 0; i < parsedData.length; i++) {
-
-    //         if (i === 0) {
-    //             parsedData[i]['timeMinutes'] = '0';
-    //             startTime = parsedData[i].time.split(':').map(x => parseInt(x));
-    //         } else {
-    //             let timeSplit = parsedData[i].time.split(':').map(x => parseInt(x));
-    //             console.log(timeSplit);
-    //             console.log(startTime);
-    //             let minuteDifference = ((timeSplit[0] - startTime[0]) * 60) + timeSplit[1];
-    //             parsedData[i]['timeMinutes'] = minuteDifference.toString();
-    //         }
-
-    //     }
-    // }
-
-    // console.log(parsedData);
-    // ownProtocol.collectionStrains['collectionData'] = parsedData;
-    // console.log(ownProtocol.collectionStrains)
 
     const renderCharts = () => {
         if (parsedData) {
@@ -78,18 +53,20 @@ const CollectionCharts = ({ ownProtocolId, laczAssayProtocols }) => {
                 parsedData.map(item => (
                     <>
                         <h3>{item.strainName}</h3>
-                        <LineChart width={600} height={400} data={item.collectionData} margin={{ top: 10, right: 30, left: 30, bottom: 40 }}>
-                            <Line type="monotone" dataKey="odValue" stroke="#8884d8" />
-                            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                            <XAxis dataKey="timeMinutes">
-                                <Label value='Minutes' position='bottom' style={{ textAnchor: 'middle' }} />
-                            </XAxis>
-                            <YAxis dataKey='odValue'>
-                                <Label value='OD600 Value' position='left' angle='-90' style={{ textAnchor: 'middle' }} />
-                            </YAxis>
-                            <Tooltip />
-                            <Legend verticalAlign="top" height={50}/>
-                        </LineChart>
+                        <ResponsiveContainer>
+                            <LineChart width={550} height={400} data={item.collectionData} margin={{ top: 10, right: 20, left: 20, bottom: 40 }}>
+                                <Line type="monotone" dataKey="odValue" stroke="#8884d8" />
+                                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                                <XAxis dataKey="timeMinutes">
+                                    <Label value='Minutes' position='bottom' style={{ textAnchor: 'middle' }} />
+                                </XAxis>
+                                <YAxis dataKey='odValue'>
+                                    <Label value='OD600 Value' position='left' angle='-90' style={{ textAnchor: 'middle' }} />
+                                </YAxis>
+                                <Tooltip />
+                                <Legend verticalAlign="top" height={50}/>
+                            </LineChart>
+                        </ResponsiveContainer>
                     </>
                 ))
             )
