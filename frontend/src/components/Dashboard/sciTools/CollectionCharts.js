792a94369c1131ca1410c2770f26e1376aa3a23d
Haz-git <zhouharry1@gmail.com>
2020-11-27 09:58:13 -0800

    Added rsq, linearRegression, and more to parsedData object and rendered onto the screen

--- frontend/src/components/Dashboard/sciTools/CollectionCharts.js
+++ frontend/src/components/Dashboard/sciTools/CollectionCharts.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Label, ResponsiveContainer } from 'recharts';
 import { StyledMainHeader } from './Collection';
+import Badge from 'react-bootstrap/Badge';
+
+//Statistics:
+import { linearRegression, linearRegressionLine, rSquared } from 'simple-statistics';
 
 //Styles:
 
@@ -26,6 +30,16 @@ const ResContainer = styled.div`
     margin-bottom: 20px;
 `
 
+const StyledBadge = styled(Badge)`
+    padding: 6px 6px;
+    margin-top: 5px;
+    margin-left: 5px;
+    margin-right: 5px;
+    background-color: navy;
+` 
+
+//Render:
+
 const CollectionCharts = ({ ownProtocolId, laczAssayProtocols }) => {
 
     const ownProtocol = laczAssayProtocols.find(item => item.protocolId === ownProtocolId);
@@ -36,6 +50,7 @@ const CollectionCharts = ({ ownProtocolId, laczAssayProtocols }) => {
         parsedData = [...ownProtocol.collectionStrains];
     }
 
+    //Calculate and create timeMinutes into parsedData object:
 
     if (parsedData !== undefined) {
         
@@ -48,6 +63,8 @@ const CollectionCharts = ({ ownProtocolId, laczAssayProtocols }) => {
             if (parsedData[i].collectionData) {
 
                 for (let j = 0; j < parsedData[i].collectionData.length; j++) {
+
+                    //Calculating timeMinutes:
                     let timeSplit = parsedData[i].collectionData[j].time.split(':').map(x => parseInt(x));
                     let minuteDifference = ((timeSplit[0] - startTime[0]) * 60) + timeSplit[1];
                     parsedData[i].collectionData[j]['timeMinutes'] = minuteDifference.toString();
@@ -56,7 +73,45 @@ const CollectionCharts = ({ ownProtocolId, laczAssayProtocols }) => {
 
             }
         }
+
+        console.log(parsedData.collectionData);
+
+        //Using new timeMinutes created in parsedData, create linearRegression and RSQ:
+        // if (parsedData.collectionData !== undefined) {
+            
+        for (let k = 0; k < parsedData.length; k++) {
+
+            let linearRegressionArray = [];
+            if (parsedData[k].collectionData !== undefined) {
+                for (let y = 0; y < parsedData[k].collectionData.length; y++) {
+
+                    let pairArray = [];
+                    pairArray.push(Math.log2(Number(parsedData[k].collectionData[y].timeMinutes)));
+                    pairArray.push(Number(parsedData[k].collectionData[y].odValue));
+
+                    console.log(pairArray);
+
+                    linearRegressionArray.push(pairArray);
+                }
+
+                parsedData[k]['linearRegressionArray'] = linearRegressionArray;
+
+                const { m } = linearRegression(linearRegressionArray);
+                const regressionLine = linearRegressionLine(linearRegression(linearRegressionArray));
+                const rSquaredValue = rSquared(linearRegressionArray, regressionLine);
+
+
+                parsedData[k]['linearRegressionValue'] = m;
+                parsedData[k]['doublingTime'] = (m * 100).toFixed(1);
+                parsedData[k]['rSquaredValue'] = rSquaredValue.toFixed(3);
+
+            }
+        }
+
     }
+
+    console.log(parsedData);
+
 
 
 
@@ -67,6 +122,18 @@ const CollectionCharts = ({ ownProtocolId, laczAssayProtocols }) => {
                     <>
                         <ResContainer>
                             <ChartHeader>{item.strainName}</ChartHeader>
+                            <div>
+                                <StyledBadge variant='dark'>
+                                    Calculated Doubling Time: {item.doublingTime} minutes
+                                </StyledBadge>
+                                <StyledBadge variant='dark'>
+                                    rSquaredValue: {item.rSquaredValue}
+                                </StyledBadge>
+                                <StyledBadge variant='dark'>
+                                    Starting OD600: {item.startingOD} |||
+                                    Starting Time: {item.startTime}
+                                </StyledBadge>
+                            </div>
                             <ResponsiveContainer aspect={1.8}>
                                 <LineChart width={550} height={400} data={item.collectionData} margin={{ top: 10, right: 20, left: 20, bottom: 40 }}>
                                     <Line type="monotone" dataKey="odValue" stroke="#8884d8" strokeWidth={2.5} />
