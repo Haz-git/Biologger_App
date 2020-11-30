f958ab56d1395aa06c71d3c879bb83db6dc24588
Haz-git <zhouharry1@gmail.com>
2020-11-30 05:55:11 -0800

    Created card component to be rendered out

---
+++ frontend/src/components/Dashboard/sciTools/LacZCards.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LacZCards = ({
+    dilutionFactor,
+    volume,
+    ownProtocolId,
+    ownStrainId,
+    ownStrainName,
+    collectionData,
+}) => {
+    return (
+        <div>
+            {ownStrainName}
+        </div>
+    )
+}
+
+export default LacZCards;
