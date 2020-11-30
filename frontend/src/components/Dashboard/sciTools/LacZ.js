b71d4f675e0e7f0df201ec02cc8bb915d28321b7
Haz-git <zhouharry1@gmail.com>
2020-11-30 05:56:00 -0800

    Created Layout, basic styling, and rendering function for the LacZ assay

--- frontend/src/components/Dashboard/sciTools/LacZ.js
+++ frontend/src/components/Dashboard/sciTools/LacZ.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import LacZCards from './LacZCards';
+import styled from 'styled-components';
 
-const LacZ = () => {
+//Styles:
+import { MainHeader, SecondaryHeader } from '../../signupPage/SignUpForm';
+
+const LacZMainContainer = styled.div`
+    padding: 20px 20px;
+    text-align: center;
+`
+
+//Render:
+
+const LacZ = ({ laczAssayProtocols, match:{params:{id}} }) => {
+
+    const ownProtocol = laczAssayProtocols.find(x => x.protocolId === id);
+
+    console.log(ownProtocol)
+
+    const renderLacZCards = () => {
+        if (ownProtocol.collectionStrains !== null && ownProtocol.collectionStrains !== undefined) {
+            return ownProtocol.collectionStrains.map(strain => (
+                <LacZCards
+                    dilutionFactor={5}
+                    volume={0.05}
+                    ownProtocolId={ownProtocol.protocolId}
+                    ownStrainId={strain.strainId}
+                    ownStrainName={strain.strainName}
+                    collectionData={strain.collectionData}
+                />
+            ))
+        } else {
+            return (
+                <SecondaryHeader>
+                    Sorry! You haven't collected any strains yet. Try to do that before running the LacZ assay. It's the first step, after all.
+                </SecondaryHeader>
+            )
+        }
+    }
+
+
     return (
-        <div>
-            lacZ page
-        </div>
+        <LacZMainContainer>
+            <div>
+                <MainHeader>LacZ Data Collection</MainHeader>
+            </div>
+            <div>
+                {renderLacZCards()}
+            </div>
+        </LacZMainContainer>
     )
 }
 
-export default LacZ;
+const mapStateToProps = state => {
+    return {
+        laczAssayProtocols: state.laczAssayProtocols.laczProtocol,
+    }
+}
+
+export default connect(mapStateToProps)(LacZ);
