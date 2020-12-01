a38e3def7cfbef69cc7aee4633daf1ed5d0af5dd
Haz-git <zhouharry1@gmail.com>
2020-12-01 12:15:56 -0800

    test

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL: 'http://localhost:8080/api',
-    baseURL: 'https://petri-webapp-heroku.herokuapp.com/',
+    baseURL: 'https://petri-webapp-heroku.herokuapp.com/api',
     withCredentials: true,
 });
