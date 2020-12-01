6234c1e55feeb836358fea6a1f043428ae1c6f0c
Haz-git <zhouharry1@gmail.com>
2020-12-01 12:17:20 -0800

    Updates

---
+++ precache-manifest.af85b056b3f95638fa5cb9ebd450572d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e19eeecadd331efac1dbd361eb89c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b432036210a2d1ee08a",
+    "url": "/static/css/2.0c6eb27b.chunk.css"
+  },
+  {
+    "revision": "2b432036210a2d1ee08a",
+    "url": "/static/js/2.b70cd4b6.chunk.js"
+  },
+  {
+    "revision": "1c711041d61b9326fada9c6d0b0db99c",
+    "url": "/static/js/2.b70cd4b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "355ef96a73b3b275bf56",
+    "url": "/static/js/main.fafc4bb8.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "b8f165ff2c9c1f1f1b1ed35c62632ad2",
+    "url": "/static/media/bgalact.b8f165ff.png"
+  },
+  {
+    "revision": "c6d1bede875dd30c090107a4e0c21ffa",
+    "url": "/static/media/petriLogoEdit2.c6d1bede.png"
+  },
+  {
+    "revision": "e0591076038b15d20e6c8cc72f6724c2",
+    "url": "/static/media/signupImg.e0591076.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Petri/precache-manifest.b92e601b13772238e43d765e17996240.js"
+  "/precache-manifest.af85b056b3f95638fa5cb9ebd450572d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Petri/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
